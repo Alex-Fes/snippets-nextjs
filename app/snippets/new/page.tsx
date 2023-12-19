fa3d33c0b7fba4344d913de40c2957ee7b3709b1
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-19 20:32:27 +0100

    move createSnippet to actions.ts, add validation, add useFormState to CreateSnippetPage,

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,29 +1,16 @@
-import {db} from "@/src/db";
-import {redirect} from "next/navigation";
+'use client'
 
+import {useFormState} from "react-dom";
+import * as actions from "@/src/actions";
 
 export default function CreateSnippetPage() {
-    async function createSnippet(formData: FormData) {
-        'use server'
 
-        const title = formData.get('title') as string
-        const code = formData.get('code') as string
-
-        const snippet = await db.snippet.create({
-            data: {
-                title,
-                code,
-            },
-        })
-        console.log(snippet)
-
-        redirect('/')
-    }
+   const [formState, action] = useFormState(actions.createSnippet, {message: ''})
 
 
 
     return (
-        <form action={createSnippet}>
+        <form action={action}>
             <h3 className={'font-bold m-3'}>Create a new snippet</h3>
             <div className={'flex flex-col gap-4'}>
                 <div className={'flex gap-4'}>
@@ -45,6 +32,8 @@ export default function CreateSnippetPage() {
                         id="code"
                         className={'border border-gray-400 rounded p-2 w-full'}/>
                 </div>
+
+               {formState.message ? <div className={'p-2 bg-red-200 border rounded border-red-400'}>{formState.message}</div> : null}
                 <button type={'submit'} className={'rounded p-2 bg-blue-200'}>
                     Create
                 </button>
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -5,8 +5,8 @@ import {redirect} from "next/navigation";
 
 export async function editSnippet(id: number, code: string) {
     await db.snippet.update({
-        where: { id },
-        data: { code },
+        where: {id},
+        data: {code},
     })
 
     redirect(`/snippets/${id}`)
@@ -14,8 +14,34 @@ export async function editSnippet(id: number, code: string) {
 
 export async function deleteSnippet(id: number) {
     await db.snippet.delete({
-        where: { id },
+        where: {id},
     })
+
+    redirect('/')
+}
+
+export async function createSnippet(formState: { message: string }, formData: FormData) {
+    const title = formData.get('title')
+    const code = formData.get('code')
+
+    if (typeof title !== 'string' || title.length < 3) {
+        return {
+            message: 'Title must be at least 3 characters long',
+        }
+    }
+    if (typeof code !== 'string' || code.length < 10) {
+        return {
+            message: 'Code must be at least 10 characters long',
+        }
+    }
+
+    const snippet = await db.snippet.create({
+        data: {
+            title,
+            code,
+        },
+    })
+    console.log(snippet)
 
     redirect('/')
 }
