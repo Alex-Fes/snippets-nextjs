d222e4a23e07ad08252fd0051c22cda69c9cd7fa
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-19 18:36:08 +0100

    add snippet-edit-form.tsx, monaco-editor

--- app/snippets/[id]/edit/page.tsx
+++ app/snippets/[id]/edit/page.tsx
@@ -1,3 +1,6 @@
+import {db} from "@/src/db";
+import {notFound} from "next/navigation";
+import SnippetEditForm from "@/src/components/snippet-edit-form";
 
 interface EditSnippetPageProps {
     params: {
@@ -5,11 +8,19 @@ interface EditSnippetPageProps {
     }
 }
 
-export default function EditSnippetPage(props: EditSnippetPageProps) {
+export default async function EditSnippetPage(props: EditSnippetPageProps) {
     const snippetId = parseInt(props.params.id)
-    return (
-        <div>
-            <h1>Edit a snippet - {snippetId}</h1>
-        </div>
-    )
+    const snippet = await db.snippet.findFirst({
+        where: {
+            id: snippetId
+        }
+    })
+
+    if (!snippet) {
+        return notFound()
+    }
+
+    return <div>
+        <SnippetEditForm snippet={snippet}/>
+    </div>
 }
