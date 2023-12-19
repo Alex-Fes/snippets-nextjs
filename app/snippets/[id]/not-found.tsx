b8227a8a00bcb116ed0d5acbd7f7143b6e4c1ac0
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-19 13:18:53 +0100

    add not found page

---
+++ app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function SnippetNotFoundPage() {
+    return (
+        <div>
+            <h1 className={'text-xl bold accent-red-400'}>Snippet not found</h1>
+        </div>
+    )
+}
